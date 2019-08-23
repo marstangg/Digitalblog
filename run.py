7c26d19b115a2b13d91c8c2028d7c56095c648aa
Ubuntu <ubuntu@ip-172-31-93-180.ec2.internal>
2019-08-23 21:25:58 +0000

    Home.html route created and working

---
+++ run.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+
+# adding in functionality to access the operating system
+import os
+
+app = Flask(__name__)
+
+@app.route('/home')
+def foobar():
+    return render_template("home.html")
+
+@app.route('/greet_the_person', methods=['POST'])
+def process_form():
+    print(request.form)
+    n = request.form.get('person_name')
+    age = request.form.get('age')
+    return "Hi, {}, welcome to the website!".format(n)
+
+
+# @app.route('/hello', methods=['GET', 'POST'])
+# def foobar():
+#     if request.method == 'GET':
+#         return render_template("hello.html")
+#     elif request.method == 'POST':
+#         print(request.form)
+#         n = request.form.get('person_name')
+#         age = request.form.get('age')
+#         return "Hi, {}, welcome to the website".format(n)
+
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True) 
