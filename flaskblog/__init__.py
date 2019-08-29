7eae4d46ef7045b0546176195b5a72b763e4096e
Your Name <ubuntu@ip-172-31-93-180.ec2.internal>
2019-08-29 12:44:30 +0000

    deployment settings

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
 from flask_login import LoginManager
+import os
+import psycopg2
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '37d9b26f4a0cda72818c4c6d4e3a9641'
@@ -11,5 +14,6 @@ bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
 
 from flaskblog import routes
